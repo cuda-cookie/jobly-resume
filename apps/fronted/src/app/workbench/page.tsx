3ae3fbee40a8cbf3c5b238ecc00ce083d16f6243
cookie-may <cookie-may@users.noreply.github.com>
2024-06-11 23:59:58 +0800

    feat: general development update

--- apps/fronted/src/app/workbench/page.tsx
+++ apps/fronted/src/app/workbench/page.tsx
@@ -1,13 +1,6 @@
 "use client";
-import Editor from "./compoents/Editor";
-import Preview from "./compoents/Preview";
 const WorkBench = () => {
-  return (
-    <div>
-      <Editor></Editor>
-      <Preview></Preview>
-    </div>
-  );
+  return <div>WorkBench</div>;
 };
 
 export default WorkBench;
