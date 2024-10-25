8d6fc5d62fd69dcda0d8fcaee91d07ed6896381a
cookie-may <cookie-may@users.noreply.github.com>
2024-10-25 22:04:04 +0800

    feat: general development update

--- apps/fronted/src/app/workbench/page.tsx
+++ apps/fronted/src/app/workbench/page.tsx
@@ -1,32 +1,6 @@
 "use client";
-import Editor from "./compoents/Editor";
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup
-} from "@/components/ui/resizable";
-import Preview from "./compoents/Preview";
-import styles from "./index.module.scss";
 const WorkBench = () => {
-  return (
-    <div className={styles.container}>
-      <ResizablePanelGroup
-        direction="horizontal"
-        className="h-[100vh] rounded-lg border "
-      >
-        <ResizablePanel defaultSize={50} className="max-w-[60%] min-w-[20%]">
-          <Editor></Editor>
-        </ResizablePanel>
-        <ResizableHandle
-          withHandle
-          className="hover:w-[2px] hover:bg-[#80ed99]"
-        />
-        <ResizablePanel>
-          <Preview></Preview>
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </div>
-  );
+  return <div>WorkBench</div>;
 };
 
 export default WorkBench;
